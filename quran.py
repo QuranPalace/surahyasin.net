dca7df2ca693c55c41bf01c89e24be8756a8d951
Meisam Ahmadi <me.ahmadi@gmail.com>
2016-02-22 11:37:59 +0330

    add line by line quran

--- quran.py
+++ quran.py
@@ -1,12 +1,20 @@
-from flask import Flask
+#-*- coding: utf-8 -*-
+import glob,re
+
+from flask import Flask,url_for
 app = Flask(__name__)
 
 @app.route('/surah/<int:surahnumber>')
 def surah(surahnumber):
-    if surahnumber== 36:
-        print ":)"
-    else:
-        print ":("
+	result = u"<center>"
+	x = re.compile(r'static\\ayatimages\\%d_([0-9]*).png'%surahnumber)
+	print glob.glob('static\\ayatimages\\%d_*.png'%surahnumber)
+	ayat = sorted([ (int(x.search(f).group(1)),f) for f in glob.glob('static\\ayatimages\\%d_*.png'%surahnumber) ], key=lambda t:t[0])
+	for a in ayat:
+		result += "<img width=80% src=\""+url_for("static",filename="ayatimages/%d_%d.png"%(surahnumber,a[0]))+"\"/></br>\n"
+	result +="</center>"
+	return result
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=true)
+	app.run(host='0.0.0.0', debug=True)
